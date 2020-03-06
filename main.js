f2640fcca312734ec34173377ac742007b2016cf
Raya Anderson <rayazand@gmail.com>
2020-03-05 21:41:45 -0800

    lib folder is no longer needed

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-$().ready(function () {
+$(document).ready(function () {
     $('menu.toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
