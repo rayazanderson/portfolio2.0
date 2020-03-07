f59a3f6535085feef41919697feef6a4c8a5e0bf
Raya Anderson <rayazand@gmail.com>
2020-03-07 00:16:53 -0800

    adding css to skills/services and portfolio

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-    $('menu.toggler').on('click', function () {
+    $('.menu-toggler').on('click', function () {
         $(this).toggleClass('open');
         $('.top-nav').toggleClass('open');
     });
